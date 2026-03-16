083e34c1e04a79418470091f339221ea0e8b4da6
Lexel Yu <Lexel_Yu@asus.com>
2026-03-16 22:46:26 +0800

    Update Hello.c

--- Hello.c
+++ Hello.c
@@ -1,56 +1,75 @@
 /*
- * This is a test program
+ * This is a test program demonstrating basic C functionality
+ * including I/O operations, macros, and utility functions.
  *
+ * Author: lexel225
+ * Version: 2.0
 **/
 
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <time.h>
+#include <stdio.h>   // Standard I/O library (printf, scanf)
+#include <string.h>  // String functions (strlen)
+#include <stdlib.h>  // Standard library functions (rand, srand)
+#include <time.h>    // Time functions (clock)
 
+// Boolean definitions
 #define TRUE (1)
 #define FALSE (0)
 
+// Macro to return the maximum of two values
 #define MAX(x,y) (x>=y?x:y)
 
+// Generates and outputs a random number based on user input
+// Prompts user for maximum value, then outputs a random number from 0 to max
 void Output_random_number(void)
 {
 	unsigned int max;
 
+	// Seed the random number generator with current time
 	srand((unsigned int)clock());
 	printf("Input Maximal number:");
 	scanf("%d", &max);
 
+	// Output random number only if max is not zero
 	if (max != 0)
 		printf("Output random number: %d\n", rand() % max);
 	else
 	{
+		// Error case: user entered zero
 		printf("Input zero!!");
 	}
 	
 }
 
+// Reads a string from user input and outputs its length
 void Output_string_length(void)
 {
-	char input_str[1024];
+	char input_str[1024];  // Buffer to store input string (1024 bytes)
 
 	printf("Input string:");
 	scanf("%s", input_str);
-//	printf("Output string: %s\n", input_str);
+	// Debug: Uncomment below to also output the string itself
+	// printf("Output string: %s\n", input_str);
 	printf("Output string length: %d\n", (unsigned int)strlen(input_str));
 }
 
-// Function description v2
+// Main entry point - demonstrates basic C functionality
+// Tests macro functionality and string output
 int main(void)
 {
+	// Output greeting message
 	printf("Hello World\n");
 
-//	Output_random_number();
+	// Note: The following functions are commented out
+	// Uncomment below to test random number generation
+	// Output_random_number();
 
-//	Output_string_length();
+	// Uncomment below to test string length function
+	// Output_string_length();
 
+	// Demonstrate MAX macro with sample values
 	printf("MAX(1000,1)=%d\n", MAX(1000,1));
 	
+	// Output farewell message
 	printf("Good Bye!!!\n");
 
 	return TRUE;
